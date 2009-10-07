e09e8d4e31cb32c65a608091ed7090bd4f0bf03f
olivier <stagiaire@olivier.local>
2009-10-07 12:41:03 +0200

    test symlink deploy

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
 echo "hello world of deploy";
-echo "path : ".$_SERVER['REQUEST_URI'];
 
+echo "<br/> test symlink pour image durant le deploy <br/>";
 ?>
 
 <div><IMG SRC="/images/logo_noodle.jpg" ALT="logo noodle" TITLE="noodle"></div>
