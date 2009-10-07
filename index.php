0685a408f227814f38dd0e4f2de939ea629be7a9
olivier <stagiaire@olivier.local>
2009-10-07 12:46:20 +0200

    deuxiem test symlink deploy

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 echo "hello world of deploy";
 
-echo "<br/> test symlink pour image durant le deploy <br/>";
+echo "<br/> deuxieme test symlink pour image durant le deploy <br/>";
 ?>
 
 <div><IMG SRC="/images/logo_noodle.jpg" ALT="logo noodle" TITLE="noodle"></div>
