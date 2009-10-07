1fc7d64b5bd76e65dc7d7dba126b770ca71f7cad
olivier <stagiaire@olivier.local>
2009-10-07 13:01:24 +0200

    symlink3

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 echo "hello world of deploy";
 
-echo "<br/> deuxieme test symlink pour image durant le deploy <br/>";
+echo "<br/> troiseme test symlink pour image durant le deploy <br/>";
 ?>
 
 <div><IMG SRC="/images/logo_noodle.jpg" ALT="logo noodle" TITLE="noodle"></div>
