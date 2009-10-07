a3d308e66d1e865de78d82cabe5ecdc64678e89e
olivier <stagiaire@olivier.local>
2009-10-07 15:01:07 +0200

    premier commit sur branch staging

--- index.php
+++ index.php
@@ -11,3 +11,6 @@ echo "<br/> troiseme test symlink pour image durant le deploy <br/>";
 	ln -s /vhome/vhosts/dev.kantik.net/htdocs/olivier/test_deploy/asset/images /vhome/vhosts/dev.kantik.net/htdocs/olivier/test_deploy/current/images
 </div>
 
+<h1>Creer un nouvelle branche distante</h1>
+<div> git remote add nombranch repertoireGithub </div>
+<div> git push origin nomBranch </div>
